e8c608637a0e3adc9cf9308a08f33ae3b6e9fff0
GuilhermeVGalvao <guilhermevigalvao@hotmail.com>
2022-02-04 15:35:08 -0300

    Main file

--- wihunter.py
+++ wihunter.py
@@ -5,6 +5,29 @@ import netmanager
 import wifite_attacker
 from printer import dialog
 
+################################################################################
+###                                                                          ###
+###           WIHUNTER HAS BEEN CREATED BY GUILHERME VIEIRA GALVÃO           ###
+###                   GITHUB: github.com/GuilhermeVGalvao/                   ###
+###                                                                          ###
+###                             GuilhermeVGalvao                             ###
+###                          Instagram: @galvao.exe                          ###
+###                           Twitter: @galvao_exe                           ###
+###                                                                          ###
+###--------------------------------------------------------------------------###
+###                                                                          ###
+### Wihunter is a free software: You can use, redistribute and/or modify     ###
+###                                                                          ###
+### Whihunter uses Aircrack and Wifite Tecnologies, you can find them in:    ###
+### https://www.aircrack-ng.org/ and                                         ###
+### https://github.com/kimocoder/wifite2                                     ###
+###                                                                          ###
+###--------------------------------------------------------------------------###
+###                                                                          ###
+###              "With great power comes greate responsibility"              ###
+###                                                                          ###
+################################################################################
+
 ### Variáveis Globais ###
 INTERFACE = ''
 AIRODUMP_TIMEOUT = 10
