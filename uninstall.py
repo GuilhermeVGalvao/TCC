14b7e515c4301bd38f123370ade972a60a506d7c
GuilhermeVGalvao <guilhermevigalvao@hotmail.com>
2022-02-04 19:21:53 -0300

    Uninstaller

--- uninstall.py
+++ uninstall.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
 import os
+import sys
+
+lr = '\033[31m'
 
 def main():
+    if not os.getuid() == 0:
+        sys.exit(f'{lr}[*] O desinstalador deve ser executado como superusuário!')
+
     # Delete program directory and files
     os.system( 'rm -rf /opt/wihunter/' )
 
